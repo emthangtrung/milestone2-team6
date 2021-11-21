402c9669fb767f52ff3f1f668334aa8e9bfd4ceb
Danh Pham <danhpham@Danhs-MacBook-Pro.local>
2021-11-21 12:56:53 -0800

    edit todolist.html

--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,13 @@
 from myapp import db
 
+#to do list db
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
 db.create_all()
+
     
 
     
