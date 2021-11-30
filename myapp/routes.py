833e6ed3612c9b7f910af52eda42014bf853dad1
Thinh Vo <nghe_thuat20@yahoo.com>
2021-11-29 20:46:46 -0800

    Add docstring comments
    Add docstring comments for all cases

--- myapp/routes.py
+++ myapp/routes.py
@@ -24,11 +24,21 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Home
 @myapp_obj.route('/', methods=['GET'])
 def home():
+    """
+    Return:
+	    home page if user logged in 
+    """
     return render_template('home.html')
 
 # Signup
 @myapp_obj.route('/signup', methods=['GET', 'POST'])
 def signup():
+    """
+    Navigate to sign up page 
+    
+    Returns:
+	  redirecting back to the login page
+    """
     form = SignupForm()
     # Check if the form is validated on submission
     if form.validate_on_submit():
@@ -58,6 +68,14 @@ def signup():
 # Login
 @myapp_obj.route('/login', methods=['GET', 'POST'])
 def login():
+    """
+    Navigate to log in page 
+ 
+    Returns:
+	    redirecting back to the page
+     or 
+	    redirecting to input flashcard page
+    """
     form = LoginForm()
     # Check form validation
     if form.validate_on_submit():
@@ -77,12 +95,24 @@ def login():
 @myapp_obj.route("/logout")
 @login_required
 def logout():
+    """
+    Log out and redirect back to the login page
+         
+    Returns:
+        redirecting back to the log in page
+    """
     logout_user()
     return redirect(url_for('home'))
 # Input FlashCards
 @myapp_obj.route("/input-flashcards", methods=['GET', 'POST'])
 @login_required
 def input_flashcards():
+    """
+    This function will help user add flashcard
+
+    Return:
+	    redirecting back to the input flashcard page
+    """
     form = FlashCardForm()
     user_flashcards = FlashCard.query.filter_by(user=current_user).all()
     if form.validate_on_submit():
@@ -97,6 +127,12 @@ def input_flashcards():
 @myapp_obj.route("/delete-flashcard/<int:id>")
 @login_required
 def delete_flashcard(id):
+    """
+    This function will help user delete the flash card which is inputed by user
+
+    Return:
+	    redirecting back to the input flashcard page
+    """
     flashcard = FlashCard.query.get_or_404(id)
     if flashcard.user != current_user:
         abort(403)
@@ -111,6 +147,12 @@ def renderFlashCard():
     return render_template("renderFlashCard.html")
 @myapp_obj.route("/convert", methods=["POST"])
 def renderMarkdown():
+    """
+    [This methods is taking markdown file and convert it into html ]
+
+    Returns:
+        [HTML file]: [convert from markdown file to html file]
+    """
     read_file = open("Readme.md", encoding="utf8")
     md_template_string = markdown.markdown(
           read_file.read(), extensions=["fenced_code"]
@@ -126,11 +168,23 @@ def renderMarkdown():
 #start todo list page
 @myapp_obj.route("/todolist")
 def todoList():
+    """
+    This function help user add todo list
+
+    Returns:
+        their task list with complete button
+        
+    """
     todo_list = Todo.query.all()
     return render_template("todolist.html", todo_list=todo_list)
 
 @myapp_obj.route("/add", methods=["POST"])
 def add():
+    """Add event to data base
+
+    Returns:
+        back to todolist page
+    """
     title = request.form.get("title")
     new_todo = Todo(title=title, complete=False)
     db.session.add(new_todo)
@@ -139,6 +193,11 @@ def add():
 
 @myapp_obj.route("/update/<int:todo_id>")
 def update(todo_id):
+    """Update event to data base
+    
+    Returns:
+        back to todolist page
+    """
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
     db.session.commit()
@@ -146,6 +205,11 @@ def update(todo_id):
 
 @myapp_obj.route("/delete/<int:todo_id>")
 def delete(todo_id):
+    """Delete event from data base
+
+    Returns:
+        back to todolist page
+    """
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
     db.session.commit()
@@ -156,6 +220,13 @@ def delete(todo_id):
 #start pomorodo timer page
 @myapp_obj.route("/pomorodo")
 def timer():
+    """
+    This function help user start studying time
+
+    Returns:
+        25mins countdown
+        
+    """
     return render_template("pomorodo.html")
 #end pomorodo timer page
 
@@ -165,10 +236,21 @@ def orders():
     return render_template("download.html")
 @myapp_obj.route("/dir", methods=["POST"])
 def dlPdf():
+    """
+    [This function is taking an html file and 
+        convert it into pdf. Download available]
+
+    Returns:
+        [Pdf file]: [a pdf file with button to download]
+    """
+    # add wkhtmltopdf local path when download wkhtmltopdf from its website
     path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+    # read html file
     html = render_template("download.html")
+    # convert to pdf
     config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
     pdf = pdfkit.from_string(html, False, configuration=config)
+    # return pdf file
     response = make_response(pdf)
     response.headers["Content-Type"] = "application/pdf"
     response.headers["Content-Disposition"] = "inline; filename=output.pdf"
@@ -179,6 +261,14 @@ def dlPdf():
 @myapp_obj.route("/create-event", methods=['GET','POST'])
 @login_required
 def create_event():
+    """user can add a event on specific date. 
+        The event gets saved onto the data base
+
+    Returns:
+        lets user know their event has been added. 
+        Button is shown so user can go to calender.
+        Show their event block on the calender
+    """
     form = EventsForm()
     user_events = Events.query.filter_by(user=current_user).all()
     if form.validate_on_submit():
@@ -193,4 +283,9 @@ def create_event():
 @myapp_obj.route("/calendar-view", methods=['GET', 'POST'])
 @login_required
 def calender_view():
+    """user can see their even block on the calendar once they have made one
+
+    Returns:
+        back to viewCalendarV2 page
+    """
     return render_template("viewCalendarV2.html")
